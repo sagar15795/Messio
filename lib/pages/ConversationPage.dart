5d9400eddb2429831a49bb2282e08675f2f2c05f
Sagar <kumarsagar15795@gmail.com>
2019-10-30 10:52:55 +0530

    feat; Added slideable widget using bottomsheet

--- lib/pages/ConversationPage.dart
+++ lib/pages/ConversationPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:messio/config/Palette.dart';
+import 'package:messio/pages/ConversationBottomSheet.dart';
 import 'package:messio/widgets/ChatAppBar.dart';
 import 'package:messio/widgets/ChatListWidget.dart';
 import 'package:messio/widgets/InputWidget.dart';
@@ -6,28 +8,37 @@ import 'package:messio/widgets/InputWidget.dart';
 class ConversationPage extends StatefulWidget {
   @override
   _ConversationPageState createState() => _ConversationPageState();
+
   const ConversationPage();
 }
 
 class _ConversationPageState extends State<ConversationPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+            key: _scaffoldKey,
             appBar: ChatAppBar(), // Custom app bar for chat screen
-            body: Stack(children: <Widget>[
-              Column(
-                children: <Widget>[
-                  ChatListWidget(),//Chat list
-                  InputWidget() // The input widget
-                ],
-              ),
-            ]
-            )
-        )
-    );
+            body: Container(
+                color: Palette.chatBackgroundColor,
+                child: Stack(children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      ChatListWidget(),
+                      GestureDetector(
+                          child: InputWidget(),
+                          onPanUpdate: (details) {
+                            if (details.delta.dy < 0) {
+                              _scaffoldKey.currentState.showBottomSheet<Null>(
+                                      (BuildContext context) {
+                                    return ConversationBottomSheet();
+                                  });
+                            }
+                          })
+                    ],
+                  ),
+                ]))));
   }
-
-
 }
