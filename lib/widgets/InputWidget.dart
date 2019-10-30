5c19598bce19480bc2c3f155eae72193a92e3b4d
Sagar <kumarsagar15795@gmail.com>
2019-10-30 10:47:29 +0530

    refactor: Clubbing the style using Styles file

--- lib/widgets/InputWidget.dart
+++ lib/widgets/InputWidget.dart
@@ -1,62 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:messio/config/Palette.dart';
+import 'package:messio/pages/ConversationBottomSheet.dart';
 
 class InputWidget extends StatelessWidget {
+  final TextEditingController textEditingController = TextEditingController();
 
-  final TextEditingController textEditingController =  TextEditingController();
   InputWidget();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: <Widget>[
-          Material(
-            child: new Container(
-              margin: new EdgeInsets.symmetric(horizontal: 1.0),
-              child: new IconButton(
-                icon: new Icon(Icons.face),
-                color: Palette.primaryColor,
-                onPressed: ()=>{},
-              ),
-            ),
-            color: Colors.white,
-          ),
-
-          // Text input
-          Flexible(
-            child: Container(
-              child: TextField(
-                style: TextStyle(color: Palette.primaryTextColor, fontSize: 15.0),
-                controller: textEditingController,
-                decoration: InputDecoration.collapsed(
-                  hintText: 'Type a message',
-                  hintStyle: TextStyle(color: Palette.greyColor),
+    return Material(
+        elevation: 60.0,
+        child: Container(
+          child: Row(
+            children: <Widget>[
+              Material(
+                child: new Container(
+                  margin: new EdgeInsets.symmetric(horizontal: 1.0),
+                  child: new IconButton(
+                    icon: new Icon(Icons.face),
+                    color: Palette.accentColor,
+                    onPressed: () => {
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (BuildContext bc) {
+                            return Container(
+                              child: new Wrap(
+                                children: <Widget>[ConversationBottomSheet()],
+                              ),
+                            );
+                          })
+                    },
+                  ),
                 ),
+                color: Colors.white,
               ),
-            ),
-          ),
 
-          // Send Message Button
-          Material(
-            child: new Container(
-              margin: new EdgeInsets.symmetric(horizontal: 8.0),
-              child: new IconButton(
-                icon: new Icon(Icons.send),
-                onPressed: () => {},
-                color: Palette.primaryColor,
+              // Text input
+              Flexible(
+                child: Material(
+                    child: Container(
+                  child: TextField(
+                    style: TextStyle(
+                        color: Palette.primaryTextColor, fontSize: 15.0),
+                    controller: textEditingController,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Type a message',
+                      hintStyle: TextStyle(color: Palette.greyColor),
+                    ),
+                  ),
+                )),
               ),
-            ),
-            color: Colors.white,
+
+              // Send Message Button
+              Material(
+                child: new Container(
+                  margin: new EdgeInsets.symmetric(horizontal: 8.0),
+                  child: new IconButton(
+                    icon: new Icon(Icons.send),
+                    onPressed: () => {},
+                    color: Palette.accentColor,
+                  ),
+                ),
+                color: Colors.white,
+              ),
+            ],
           ),
-        ],
-      ),
-      width: double.infinity,
-      height: 50.0,
-      decoration: new BoxDecoration(
-          border: new Border(
-              top: new BorderSide(color: Palette.greyColor, width: 0.5)),
-          color: Colors.white),
-    );
+          width: double.infinity,
+          height: 50.0,
+          decoration: new BoxDecoration(
+              border: new Border(
+                  top: new BorderSide(color: Palette.greyColor, width: 0.5)),
+              color: Colors.white),
+        ));
   }
 }
